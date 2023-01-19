7547f453d75f95ae7570682f5dd59aa55a39543f
Sagar Dharmanath Avhad <avhadsagar00@gmail.com>
2023-01-19 10:12:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './components/Header';
+import './App.css';
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import Cart from './components/Cart';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import  {CartState}  from '../context/Context'
+import SingleProducts from './SingleProducts';
+import Filters from './Filters';
+import './styles.css'
+
+const Home = () => {
+    const {state:{products}} = CartState()
+    console.log(products);
+  return (
+    <div className='home'>
+      <Filters />      
+      <div className='productContainer'>
+        {products.map((prod)=>{
+          return <SingleProducts prod={prod} key={prod.id}/>;
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Home
